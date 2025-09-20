eb72c4c127e1c5e51a65f016d2d39cddd8356541
Coyot23 <69750820+Coyot23@users.noreply.github.com>
2025-09-20 01:08:41 +0100

    Add files via upload

---
+++ script/c92000999.lua
@@ -0,0 +1,14 @@
+--[[Hyperlink Blocked]]
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Win(1-tp,0x6)
+end
